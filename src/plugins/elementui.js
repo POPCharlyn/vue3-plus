e8ae1fee5c64a616d1ba1b6d8fdf5a1a6f993722
POPCharlyn <1540912102@qq.com>
2022-11-19 17:09:49 +0800

    Error:ElementPlus part

---
+++ src/plugins/elementui.js
@@ -0,0 +1,12 @@
+/*
+ * 2022/11/19 17:00
+ * author: francesca
+ * email: 575525869@qq.com
+ * elementui.js
+ */
+import { ElButton, ElSwitch } from 'element-plus'
+
+export default ( app ) => {
+    app.use( ElButton )
+    app.use( ElSwitch )
+}
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  css:{
-    loaderOptions:{
-      scss:{
-        // prependData:`@import "./src/styles/main.scss;`
-        additionalData:`@import "./src/styles/main.scss";`
-      }
-    }
-  }
-})
+const { defineConfig } = require( '@vue/cli-service' )
+const ElementPlus = require("unplugin-element-plus/webpack").default;
+
+module.exports = defineConfig( {
+    transpileDependencies: true,
+    css: {
+        loaderOptions: {
+            scss: {
+                // prependData:`@import "./src/styles/main.scss;`
+                additionalData: `@import "./src/styles/main.scss";`,
+            },
+        },
+    },
+    configureWebpack: {
+        plugins: [ ElementPlus() ],
+    },
+} )
