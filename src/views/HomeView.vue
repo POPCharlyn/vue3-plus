c371fe5e51562d14ef11a5cada2b36538173f3f9
POPCharlyn <1540912102@qq.com>
2022-11-19 16:44:55 +0800

    ElementPlus full

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
-    这里是测试文字
+    <h2>这里是测试文字</h2>
+    <ul>
+      <li>qwqw</li>
+      <li>qwqw</li>
+      <li>qwqw</li>
+      <li>qwqw</li>
+    </ul>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
   </div>
 </template>
 
